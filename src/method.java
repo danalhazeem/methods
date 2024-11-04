9f78698ad10933d041fb41d4958041fea5f3dd1e
danalhazeem <92695924+danalhazeem@users.noreply.github.com>
2024-11-04 10:20:47 +0300

    test

---
+++ src/method.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class method {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(3);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(4);
+        numbers.add(5);
+        System.out.println(ArrayListSum(numbers));
+
+    }
+    static int ArrayListSum(ArrayList<Integer>   numbers){
+        int sum = 0;
+        for (int number:numbers){
+            sum +=number;
+        }
+        return sum;
+    }
+    }
